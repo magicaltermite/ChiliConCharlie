827de3b437071ab8f74ab66b56ee9e2bce79e013
kommunerne <johannesdrachmann@gmail.com>
2023-03-23 15:40:13 +0100

    Testing Ground

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.CompareTag("Player"))
+            SceneManager.LoadScene("Testing");
+    }
+}
