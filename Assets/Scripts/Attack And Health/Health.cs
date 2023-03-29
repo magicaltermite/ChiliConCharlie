f7267f92cf838d1c1a62b2d8fac6c4bbab769f26
Markus Heidemann Lorensen <markuslorensen@gmail.com>
2023-03-29 21:45:43 +0200

    Created the attack and health scripts
    Created attack and health scripts alongside hitboxes

---
+++ Assets/Scripts/Attack And Health/Health.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int health = 100; // The amount of health the unit has
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0) {
+            
+        }
+    }
+
+
+    public void Damage(int damage) {
+        if(damage < 0) {
+            throw new System.ArgumentOutOfRangeException("Damage cannot be negative"); // Since we dont want to be capable of dealing negative damage, i've made the script throw an error
+        }
+
+        health -= damage; // This script is made to allow someone to damage this character
+        Debug.Log("I am dead :c");
+    }
+}
