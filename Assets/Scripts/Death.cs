375702d360cb15f6e79f67ec9ea9808000f1ce89
kommunerne <johannesdrachmann@gmail.com>
2023-03-29 20:57:34 +0200

    Whatever

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class Death : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision) {
-        SceneManager.LoadScene("SampleScene");
+        if(collision.CompareTag("Player"))
+            SceneManager.LoadScene("TestScene");
     }
 }
--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,76 +1,108 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Så snart dette script bliver sat på et gameobject vil der automatisk også blive sat en boxcollider på
+[RequireComponent(typeof(BoxCollider2D))]
 public class EnemyScript : MonoBehaviour
 {
-    public float speed;
-    public int startingPoint; 
-    public Transform[] points;
-    private int i;
+    // Reference til waypoints:
+    public List<Transform> points;
+
+    // Int værdien for det næste punkt i listen:
+    public int nextID;
     
-    Animator animator;
-    private string currentState;
+    // Værdien som tilføjes til ID for at ændre den:
+    private int idChangeValue = 1;
+    
+    // Speed of movement or flying
 
+    public float speed = 2f;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Reset()
     {
-        transform.position = points[startingPoint].position;
-        animator.GetComponent<Animator>();
+        Init();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Init()
+    {
+        // Sæt BoxCollider til trigger
+        GetComponent<BoxCollider2D>().isTrigger = true;
+
+        // Create root GameObject
+        GameObject root = new GameObject(name + "_root");
+
+        // Reset position of Root to enemy object
+        root.transform.position = transform.position;
+
+        // Set enemy object as child of root
+        transform.SetParent(root.transform);
+
+        // Create Waypoints object
+        GameObject waypoints = new GameObject("Waypoints");
+
+        // Reset waypoint position to root
+        // Make Waypoint object child of root
+        waypoints.transform.SetParent(root.transform);
+        waypoints.transform.position = root.transform.position;
+        
+
+        // Create two points (GameObjects) and reset their position to waypoints 
+        // Make the points children of waypoint
+        GameObject p1 = new GameObject("Point1"); p1.transform.SetParent(waypoints.transform);p1.transform.position = root.transform.position;
+        GameObject p2 = new GameObject("Point2"); p2.transform.SetParent(waypoints.transform);p2.transform.position = root.transform.position;
+
+        // Init points list then add the points to it:
+        points = new List<Transform>();
+        points.Add(p1.transform);
+        points.Add(p2.transform);
+
+
+    }
+
+    private void Awake()
     {
         
     }
 
-    void ChangeAnimationState(string newState)
+    private void Update()
     {
-        // Stop denne samme animation fra at overlappe den nuværende
-        if (currentState == newState)
-            return;
-        
-        // Afspil den nye animation
-        animator.Play(newState);
-        
-        // Sæt den nyafspillede animation til den nuværende animation
-        currentState = newState;
-    }
-    
-    
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(CompareTag("MovePoint")){
-            if(transform.position.y<collision.transform.position.y)
-                collision.transform.SetParent(transform);
-        }
-    }
-    
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if(CompareTag("MovePoint"))
-            collision.transform.SetParent(null);
+        MoveToNextPoint();
     }
 
-    
-    
-    private void IdleMove()
+    private void MoveToNextPoint()
     {
-        // Et IF-Statement som tjekker om punktet er blevet nået. Hvis det er nået sættes det næste punkt
-        // i arrayet som det næste mål. Hvis ikke der er flere punkter startes der forfra.
-        if(Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        // Get the next Point transform
+        Transform goalPoint = points[nextID];
+        
+        // Flip the enemy transform to look into the points direction
+
+
+        // Move the enemy towards the goal point
+        transform.position = Vector2.MoveTowards(transform.position, goalPoint.position, speed * Time.deltaTime);
+
+        // Check the distance between enemy and goal point to trigger next point
+        if (Vector2.Distance(transform.position, goalPoint.position)<0.3f)
         {
-            i++;
-            if (i == points.Length)
+            // Check if we are at the end of the line - (Make the change -1)
+            if (nextID == points.Count - 1)
+                idChangeValue = -1;
+
+            // Check if wa are at the start of the line - (Make the change +1)
+            if (nextID == 0)
+                idChangeValue = 1;
+
+            // Apply the change on the nextID
+            nextID += idChangeValue;
+            
+            if (goalPoint.transform.position.x > transform.position.x)
             {
-                i = 0;
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else
+            {
+                transform.localScale = new Vector3(1, 1, 1);
             }
         }
-
-        // Bevæger platformen mod en given position.
-        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
     }
-
 }
