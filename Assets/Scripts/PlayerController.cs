83dd14f53b966a9cfd7ade7bfaabd9f18d59a14c
MohiShafiq <mohammedashafiq@gmail.com>
2023-04-20 15:22:33 +0200

    Dashing tilføjet til MC
    Dash mechanic er blevet tilføjet

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -35,12 +35,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask wallLayer; // Used to store the wall layer 
 
 
+    private TrailRenderer dashRender; // Storing the trail renderer for the dash
+
+    [Header("Dash")]
+    [SerializeField] private float dashingVelocityX = 200; // Velocity of the dash, Serialized for easy changes
+    // [SerializeField] private float dashingVelocityY = 10f;
+    [SerializeField] private float dashingTime = 0.1f; // How long is the dash gonna last? Serialized for easy changes
+    private Vector2 dashingDir; // Storing the direction of the dash
+    private bool isDashing; // Checking if the character is dashing
+    private bool canDash = true; // Checking if the character is in a state where they CAN dash 
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        dashRender = GetComponent<TrailRenderer>();
     }
 
 
@@ -61,7 +71,49 @@ public class PlayerController : MonoBehaviour
         if(!isWallJumping) 
             Flip();
 
-    }
+        // ----------------DASH------------
+        float dashDirX = Input.GetAxis("Horizontal"); // Horizontal direction!!
+     // float dashDirY = Input.GetAxis("Vertical"); // Vertical direction!!
+     // Since update is the fastest way to check if something is happening, the input checks are placed here to make the game feel more repsonsive
+        moveInput = Input.GetAxis("Horizontal");
+
+
+        var dashInput = Input.GetButtonDown("Dash"); // Get the dash input from the input manager
+
+        if (dashInput && canDash) // If the input for the dash is pressed and you can dash...
+        {
+            isDashing = true; // .. then you will be set to "is dashing" aaand...
+            canDash = false; // ... can dash is set to false!
+
+
+
+            dashRender.emitting = true; // Dash rederer will start emitting!
+            dashingDir = new Vector2(dashDirX, 0); // And the direction is based on the input manager so directions from there
+
+
+
+            if (dashingDir == Vector2.zero) // if were pressing the dash button but no direction 
+            {
+                dashingDir = new Vector2(transform.localScale.x, 0); // then it will go by the local scale of the x axis and not use the y axis, localscale depends on which way your char is facing!
+            }
+            StartCoroutine(StopDashing());
+        }
+
+
+        if (isDashing && dashDirX < 1)
+        {
+            rb2D.AddForce(dashingDir.normalized * dashingVelocityX);
+            Debug.Log(dashingDir.normalized * dashingVelocityX);
+            return;
+        }
+
+
+        if (isGrounded)
+        {
+            canDash = true;
+        }
+    
+}
 
     void FixedUpdate() {
         // Move and jump are placed here to ensure that they are triggered with the physics engine
@@ -76,6 +128,14 @@ public class PlayerController : MonoBehaviour
             jumpCheck = false;
         }
         
+    }
+
+    private IEnumerator StopDashing() // Coroutine for when the dash is stopping
+    {
+        yield return new WaitForSeconds(dashingTime); // Wait for the amount of time a dash takes
+        Move();
+        dashRender.emitting = false; // Set the render emitting to false
+        isDashing = false; // and change the is dashing state from true to false
     }
 
     private void Move() {
