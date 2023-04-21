e7a1c34cc233374c57f394249d746ef4fe5543f4
kommunerne <johannesdrachmann@gmail.com>
2023-04-21 17:01:17 +0200

    Revert "Revert "Chibi Con Charlie + Ekstra ting til level 1""
    This reverts commit 75d3a6eb5d987c11a7fb32ca80d055cdfb8816e2.

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -1,12 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Death : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.CompareTag("Player"))
-            SceneManager.LoadScene("TestScene");
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            SceneManager.LoadScene("Level_1");
     }
 }
