addcc7adc01f570bdd31e2ff7f561ccce10b35f6
kommunerne <johannesdrachmann@gmail.com>
2023-04-21 17:12:46 +0200

    Revert "Revert "Revert "Chibi Con Charlie + Ekstra ting til level 1"""
    This reverts commit f2ccaf2de7dbb45f1882614602b8a32f2ce164c6.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 10; // Used to allow changing of the players speed
-    public float jumpForce = 10; // Used to allow changing of the players jumpheight and speed
+  
+    public float moveSpeed = 10;    // Used to allow changing of the players speed
+    public float jumpForce = 10;    // Used to allow changing of the players jumpheight and speed
 
     public Transform groundCheck;
     public LayerMask groundLayer;
 
+    private Rigidbody2D rb2D;
+
+    private bool jumpCheck;         // This variable is used to check if the jump button is pressed
+    private float moveInput;        // Used for storing the movement input, so that it can be taken from update to fixedupdate
+    private bool isGrounded;        // Used for checking if the player is touching the ground
+    private bool isFacingRight = true;  // Used for flipping the player left or right
+
     // Variables for making the wall jump work. The method comes from this video: https://www.youtube.com/watch?v=O6VX6Ro7EtA&t=237s
     public float wallSlidingSpeed = 2f;
-
-    [SerializeField] private Vector2 wallJumpingPower = new(8f, 16f);
-
-    [SerializeField]
-    private Transform
-        wallCheck; // Used to ensure that what the player is hitting is a wall, it is an empty gameobject put on the player, that is used in the WallSlide method
-
-    [SerializeField] private LayerMask wallLayer; // Used to store the wall layer 
-    private bool isFacingRight = true; // Used for flipping the player left or right
-    private bool isGrounded; // Used for checking if the player is touching the ground
-
-    private bool isWallJumping;
     private bool isWallSliding;
 
-<<<<<<< Updated upstream
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+
+    [SerializeField]
+    private Vector2 wallJumpingPower = new Vector2(8f, 16f);
+
+    [SerializeField] private Transform wallCheck; // Used to ensure that what the player is hitting is a wall, it is an empty gameobject put on the player, that is used in the WallSlide method
+    [SerializeField] private LayerMask wallLayer; // Used to store the wall layer 
+
+
     private TrailRenderer dashRender; // Storing the trail renderer for the dash
 
     [Header("Dash")]
@@ -34,42 +44,32 @@ public class PlayerController : MonoBehaviour
     private Vector2 dashingDir; // Storing the direction of the dash
     private bool isDashing; // Checking if the character is dashing
     private bool canDash = true; // Checking if the character is in a state where they CAN dash 
-=======
-    private bool jumpCheck; // This variable is used to check if the jump button is pressed
-    private float moveInput; // Used for storing the movement input, so that it can be taken from update to fixedupdate
-
-    private Rigidbody2D rb2D;
-    private float wallJumpingCounter;
-    private float wallJumpingDirection;
-    private readonly float wallJumpingDuration = 0.4f;
-    private readonly float wallJumpingTime = 0.2f;
->>>>>>> Stashed changes
 
 
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         dashRender = GetComponent<TrailRenderer>();
     }
 
 
+
     // Update is called once per frame
-    private void Update()
-    {
+    void Update() {
+
         // Since update is the fastest way to check if something is happening, the input checks are placed here to make the game feel more repsonsive
         moveInput = Input.GetAxis("Horizontal");
-
+        
         CheckIfButtonPressed(); // Checks if a button has been pressed
-        CheckIfGrounded(); // Checks if the player is on the ground
+        CheckIfGrounded();      // Checks if the player is on the ground
 
-        WallSlide(); // Checks whether the player should be sliding down a wall
-        WallJump(); // Allows the player to jump off a wall
+        WallSlide();            // Checks whether the player should be sliding down a wall
+        WallJump();             // Allows the player to jump off a wall
 
 
-        if (!isWallJumping)
+        if(!isWallJumping) 
             Flip();
-<<<<<<< Updated upstream
 
         // ----------------DASH------------
         float dashDirX = Input.GetAxis("Horizontal"); // Horizontal direction!!
@@ -114,28 +114,22 @@ public class PlayerController : MonoBehaviour
         }
     
 }
-=======
-    }
->>>>>>> Stashed changes
 
-    private void FixedUpdate()
-    {
+    void FixedUpdate() {
         // Move and jump are placed here to ensure that they are triggered with the physics engine
-
-        Move();
-
+        
+            Move();
+        
         // Used to check if the player can jump and they have pressed the jump button
-        if (jumpCheck && isGrounded)
-        {
-            // This if statement determines if the player is allowed to jump, jump check is used to check if the player has pressed the jump button and
-            // isGrounded is used to check if the player is touching the ground
+        if (jumpCheck == true && isGrounded == true) { // This if statement determines if the player is allowed to jump, jump check is used to check if the player has pressed the jump button and
+                                                       // isGrounded is used to check if the player is touching the ground
 
             rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             jumpCheck = false;
         }
+        
     }
 
-<<<<<<< Updated upstream
     private IEnumerator StopDashing() // Coroutine for when the dash is stopping
     {
         yield return new WaitForSeconds(dashingTime); // Wait for the amount of time a dash takes
@@ -146,119 +140,92 @@ public class PlayerController : MonoBehaviour
 
     private void Move() {
         if (isGrounded) {
-=======
-    private void Move()
-    {
-        if (isGrounded)
->>>>>>> Stashed changes
             // Used to make the player move
             rb2D.velocity = new Vector2(moveInput * moveSpeed, rb2D.velocity.y);
+        }
+
     }
 
-    private void CheckIfButtonPressed()
-    {
+    private void CheckIfButtonPressed() {
         // This method would be used to check for specific button presses. I imagine this could be used to check for other buttons presses too, if that should be necessary
-        if (isGrounded)
-            if (Input.GetKeyDown(KeyCode.Space))
-                jumpCheck = true;
+        if(isGrounded)
+            if (Input.GetKeyDown(KeyCode.Space)) {
+            jumpCheck = true;
+            }
     }
 
-    private void CheckIfGrounded()
-    {
+    private void CheckIfGrounded() {
         // This method is used to check if the player is on the ground. It uses a collider object on the player prefab, that checks if it collides with an object set to the ground layer
         // The method returns true if this is the case
         // If the player is on the ground, they are allowed to jump
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheck.GetComponent<CircleCollider2D>().radius,
-            groundLayer);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheck.GetComponent<CircleCollider2D>().radius, groundLayer);
+        
     }
 
 
     // These methods deal with wall Sliding and Wall jumping
-    private bool IsWalled()
-    {
-        return
-            Physics2D.OverlapCircle(wallCheck.position, 0.2f,
-                wallLayer); // Creates a small circle around the wallcheck object on the player that checks if it is hitting a gameobject on the layer "WAll"
+    private bool IsWalled() {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer); // Creates a small circle around the wallcheck object on the player that checks if it is hitting a gameobject on the layer "WAll"
     }
 
 
-    private void WallSlide()
-    {
-        if (IsWalled() && !isGrounded && moveInput != 0f)
-        {
-            // Here is checkked if the isWalled methods returns true and the player is not grounded and their current move input is not 0, meaning they are pressing a direction
+    private void WallSlide() {
 
+        if(IsWalled() && !isGrounded && moveInput != 0f) { // Here is checkked if the isWalled methods returns true and the player is not grounded and their current move input is not 0, meaning they are pressing a direction
+            
             isWallSliding = true; // Sets the wallSliding to true
-            rb2D.velocity = new Vector2(rb2D.velocity.x,
-                Mathf.Clamp(rb2D.velocity.y, -wallSlidingSpeed,
-                    float.MaxValue)); // Changes the players velocity, so they start "falling" slowly, but only so long as they are touching a wall
+            rb2D.velocity = new Vector2(rb2D.velocity.x, Mathf.Clamp(rb2D.velocity.y, -wallSlidingSpeed, float.MaxValue)); // Changes the players velocity, so they start "falling" slowly, but only so long as they are touching a wall
         }
-        else
-        {
+        else {
             isWallSliding = false; // Sets the wallsliding to false if the player is not touching a wall
         }
+
     }
 
-    private void WallJump()
-    {
-        if (isWallSliding)
-        {
-            // This if else statement is there to give the player a short window of time after they have stopped touching the wall to still walljump, making the jump feel a little more forgiving
+    private void WallJump() {
 
-            isWallJumping = false;
-            wallJumpingDirection =
-                -transform.localScale
-                    .x; // Sets the wallJump direction to the opposite of the players current direction.
-            wallJumpingCounter = wallJumpingTime;
+        if(isWallSliding) { // This if else statement is there to give the player a short window of time after they have stopped touching the wall to still walljump, making the jump feel a little more forgiving
 
-            CancelInvoke(
-                nameof(StopWallJumping)); // Cancels the stop of the wallJump if the player is sliding on a wall, since we want the player to be cabable of wallJumping
+            isWallJumping = false; 
+            wallJumpingDirection = -transform.localScale.x; // Sets the wallJump direction to the opposite of the players current direction.
+            wallJumpingCounter = wallJumpingTime;           
+
+            CancelInvoke(nameof(StopWallJumping)); // Cancels the stop of the wallJump if the player is sliding on a wall, since we want the player to be cabable of wallJumping
         }
-        else
-        {
-            wallJumpingCounter -=
-                Time.deltaTime; // this counts down the counter, so that the player has a short window of time to wallJump even after they are no longer touching the wall
+        else {
+
+            wallJumpingCounter -= Time.deltaTime; // this counts down the counter, so that the player has a short window of time to wallJump even after they are no longer touching the wall
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && wallJumpingCounter > 0f)
-        {
-            // Checks if the player presses the jump button and if the wallJumpingCounter is greater than zero, since that means the player is allowed to wallJump
+        if(Input.GetKeyDown(KeyCode.Space) && wallJumpingCounter > 0f) { // Checks if the player presses the jump button and if the wallJumpingCounter is greater than zero, since that means the player is allowed to wallJump
 
             isWallJumping = true;
-            rb2D.velocity =
-                new Vector2(wallJumpingDirection * wallJumpingPower.x,
-                    wallJumpingPower.y); // Used to handle the players jump direction and velocity
-            wallJumpingCounter =
-                0f; // Since the player cannot wallJump again after they have already jumped, this sets that counter to zero
+            rb2D.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y); // Used to handle the players jump direction and velocity
+            wallJumpingCounter = 0f;                                                                    // Since the player cannot wallJump again after they have already jumped, this sets that counter to zero
 
-            if (transform.localScale.x != wallJumpingDirection)
-            {
-                // This part of the code ensures that the player is facing the correct direction following a walljump. Might be redundant due to how the players movement is setup
+            if (transform.localScale.x != wallJumpingDirection) { // This part of the code ensures that the player is facing the correct direction following a walljump. Might be redundant due to how the players movement is setup
                 isFacingRight = !isFacingRight;
-                var localScale = transform.localScale;
+                Vector3 localScale = transform.localScale;
                 localScale.x *= -1f;
                 transform.localScale = localScale;
             }
 
-            Invoke(nameof(StopWallJumping),
-                wallJumpingDuration); // Stops the player from wallJumping if they are no longer touching the wall after a short delay
+            Invoke(nameof(StopWallJumping), wallJumpingDuration); // Stops the player from wallJumping if they are no longer touching the wall after a short delay
+
         }
     }
 
-    private void StopWallJumping()
-    {
+    private void StopWallJumping() {
         isWallJumping = false; // Stops the player from wall jumping after they have already done so
     }
 
     // TODO: Make this code work
-    private void Flip()
-    {
-        if ((isFacingRight && moveInput < 0f) || (!isFacingRight && moveInput > 0f))
-        {
-            // The if statement here checks whether the player is inputting left or right and whether the isFacingRight is true or false
+    private void Flip() {
+
+        if(isFacingRight && moveInput < 0f || !isFacingRight && moveInput > 0f) { // The if statement here checks whether the player is inputting left or right and whether the isFacingRight is true or false
             // The video where i got this from: https://www.youtube.com/watch?v=K1xZ-rycYY8
             isFacingRight = !isFacingRight;
-            var localScale = transform.localScale; // The players localScale, can be viewed under the players transform
+            Vector3 localScale = transform.localScale; // The players localScale, can be viewed under the players transform
             localScale.x *= -1f; // Flips the current localScale value to the opposite
             transform.localScale = localScale; // Flips the players localScale
         }
